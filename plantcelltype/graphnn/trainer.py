60a8de368bfc6b285619d8e187777e82f6aa3e10
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2021-06-23 09:08:06 +0200

    add graph models

--- plantcelltype/graphnn/trainer.py
+++ plantcelltype/graphnn/trainer.py
@@ -5,7 +5,7 @@ from plantcelltype.graphnn.data_loader import get_random_split
 files_path = "/home/lcerrone/PycharmProjects/plant-celltype/data/ovules-celltype-processed/**/*.h5"
 test_loader, train_loader = get_random_split(files_path)
 
-model = GraphClassification(model_name='TGCN3',
+model = GraphClassification(model_name='GCN3',
                             model_kwargs={'in_features': 13, 'out_features': 10, 'hidden_feat': [256, 256]})
-trainer = pl.Trainer()
+trainer = pl.Trainer(gpus=1)
 trainer.fit(model, train_loader, test_loader)

260299029d4fc906a2aaf3894da8fcd2106ebc1a
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2021-10-20 12:26:19 +0200

    add additional experiments

---
+++ experiments/grs_importance.py
@@ -0,0 +1,32 @@
+from pctg_benchmark.utils.io import load_yaml
+from plantcelltype.graphnn.trainer import train
+
+template_config_paths = ('./grs_importance/deeper_gcn.yaml',
+                         './grs_importance/gcn.yaml',
+                         )
+
+grs_groups = ({'train': ('trivial_grs', ), 'test': ('trivial_grs', )},
+              {'train': ('label_grs_surface', ), 'test': ('label_grs_surface', )},
+              {'train': ('label_grs_funiculum', ), 'test': ('label_grs_funiculum', )},
+              {'train': ('es_trivial_grs', ), 'test': ('es_trivial_grs', )},
+              {'train': ('es_pca_grs', ), 'test': ('es_pca_grs', )},
+              {'train': ('label_grs_surface',
+                         'label_grs_funiculum',
+                         'es_trivial_grs',
+                         'es_pca_grs'),
+               'test': ('es_trivial_grs', )},
+              {'train': ('label_grs_surface',
+                         'label_grs_funiculum',
+                         'es_pca_grs'),
+               'test': ('es_trivial_grs', )}
+              )
+
+for template_config_path in template_config_paths:
+    template_config = load_yaml(template_config_path)
+    for grs_group in grs_groups:
+        train_name, test_name = '_'.join(grs_group['train']), '_'.join(grs_group['test'])
+        template_config['logs']['name'] += f'_train:{train_name}_test:{test_name}'
+        template_config['loader']['train_dataset']['grs'] = grs_group['train']
+        template_config['loader']['val_dataset']['grs'] = grs_group['test']
+        template_config['loader']['test_dataset']['grs'] = grs_group['test']
+        train(config=template_config)
---
+++ experiments/node_grid_search.py
@@ -0,0 +1,17 @@
+from plantcelltype.graphnn.trainer import grid_search_train
+from pctg_benchmark.utils.io import load_yaml
+
+template_config_paths = ('./experiments/node_classification_grid_search/depp_gcn.yaml',
+                         './experiments/node_classification_grid_search/gat.yaml',
+                         './experiments/node_classification_grid_search/gatv2.yaml',
+                         './experiments/node_classification_grid_search/gcn.yaml',
+                         './experiments/node_classification_grid_search/gcnii.yaml',
+                         './experiments/node_classification_grid_search/tgcn.yaml',
+                         './experiments/node_classification_grid_search/tgcn_edge.yaml',
+                         )
+
+
+for path in template_config_paths:
+    config = load_yaml(path)
+    config_grid_search = config.pop('grid_search')
+    grid_search_train(config=config, kwargs=config_grid_search)

ed130842e14cb8d7a7abaf6833d933e1c8d7dd96
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2021-10-29 23:11:33 +0200

    update configs experiments

--- run_training.py
+++ run_training.py
@@ -1,4 +1,4 @@
-from plantcelltype.graphnn.trainer import train
+from plantcelltype.graphnn.trainer import train, grid_search_train
 from pctg_benchmark.utils.io import load_yaml
 
 from plantcelltype.utils.utils import parser
@@ -6,4 +6,9 @@ from plantcelltype.utils.utils import parser
 if __name__ == '__main__':
     _args = parser()
     _config = load_yaml(_args.config)
-    train(_config)
+
+    if 'grid_search' not in _config:
+        train(_config)
+    else:
+        config_grid_search = _config.pop('grid_search')
+        grid_search_train(config=_config, kwargs=config_grid_search)

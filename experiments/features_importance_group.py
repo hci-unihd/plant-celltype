0d29a4bbf141a063b555a2680cfb04f4b9519090
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2021-11-13 17:48:55 +0100

    update experiments

--- experiments/features_importance_group.py
+++ experiments/features_importance_group.py
@@ -11,15 +11,16 @@ if __name__ == '__main__':
                                )
 
     invariant = ['degree_centrality',
+                 'lrs_axis12_dot_grs',
+                 'length_axis1_grs',
+                 'length_axis2_grs',
+                 'length_axis3_grs',
                  'hops_to_bg',
+                 'pca_explained_variance_grs',
                  'lrs_axis2_angle_grs',
                  'rw_centrality',
                  'surface_um',
                  'volume_um']
-
-    approx_invariant = ['length_axis1_grs',
-                        'length_axis2_grs',
-                        'length_axis3_grs', ]
 
     not_invariant = ['com_grs',
                      'lrs_axis12_dot_grs',
@@ -29,7 +30,6 @@ if __name__ == '__main__':
                      'lrs_proj_axis1_grs',
                      'lrs_proj_axis2_grs',
                      'lrs_proj_axis3_grs',
-                     'pca_explained_variance_grs',
                      'pca_orientation_axis1_grs',
                      'pca_orientation_axis2_grs',
                      'pca_orientation_axis3_grs',
@@ -38,13 +38,10 @@ if __name__ == '__main__':
                      'pca_proj_axis3_grs'
                      ]
 
-    _features_groups = (('baseline', ()),
+    _features_groups = (('inv_not_inv', invariant + not_invariant),
+                        ('baseline', ()),
                         ('inv', invariant),
                         ('not_inv', not_invariant),
-                        ('approx', approx_invariant),
-                        ('inv_not_inv', invariant + not_invariant),
-                        ('inv_approx', invariant + approx_invariant),
-                        ('not_inv_approx', not_invariant + approx_invariant),
                         )
 
     _save_dir = '/results/plant-ct-logs/ablation/features_importance_group/'
